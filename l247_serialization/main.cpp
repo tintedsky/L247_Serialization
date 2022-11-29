369491e79df6c3e6e893a9659380708390d5c34e
tintedsky <hongbonew@gmail.com>
2022-11-28 23:04:25 -0500

    Initial Commit

---
+++ l247_serialization/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  l247_serialization
+//
+//  Created by Hongbo Niu on 2022-11-28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
