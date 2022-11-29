87ba7d3d3acd60c24df53f3fd89cab7c04d4ece2
tintedsky <hongbonew@gmail.com>
2022-11-29 00:05:47 -0500

    of stream for student and operator overloading. <<

--- l247_serialization/main.cpp
+++ l247_serialization/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Student{
+public:
+    string name;
+    int roll;
+    string branch;
+
+    friend ofstream& operator<<(ofstream& ofs, Student &s);
+};
+
+ofstream& operator<<(ofstream& ofs, Student &s)
+{
+    ofs<<s.name<<endl;
+    ofs<<s.roll<<endl;
+    ofs<<s.branch<<endl;
+    return ofs;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Student s1;
+    s1.name="Khan"; s1.roll=23; s1.branch="CS";
+    
+    ofstream ofs("Student.txt", ios::app);
+    ofs<<s1;
+    ofs.close();
+    
     return 0;
 }
